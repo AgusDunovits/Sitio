b1a3514cb176d4d37363ecc32af38970f459c10c
IgnacioDunovits <IgnacioDunovits@hotmail.com>
2022-06-06 03:52:48 -0300

    1

--- app/src/main/java/com/example/sitio/Fragmento_casa.java
+++ app/src/main/java/com/example/sitio/Fragmento_casa.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
+import android.os.Handler;
 import android.text.Layout;
 import android.util.Log;
 import android.view.DragEvent;
@@ -24,6 +26,7 @@ import android.widget.LinearLayout;
 import android.widget.GridLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.util.TypedValue;
 
 import org.w3c.dom.Text;
 
@@ -44,6 +47,10 @@ public class Fragmento_casa extends Fragment {
     private String mParam2;
     private int int_scrollX = 0;
     private HorizontalScrollView HSV_pantalla;
+    Resources r;
+    int int_screenWidth;
+    Boolean runActivo = false;
+
     Long tsLong = 0L;
 
     public Fragmento_casa() {
@@ -67,9 +74,6 @@ public class Fragmento_casa extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-    public void test(){
-
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,17 +82,42 @@ public class Fragmento_casa extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
-    @Override
+    final Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Long Long_temp = System.currentTimeMillis()/300;
+            if(Long_temp-tsLong>1 ) {
+                if(!runActivo) {
+                    tsLong = Long_temp;
+                    int temp_scrollX = HSV_pantalla.getScrollX();
+                    int int_anchoPromedio = temp_scrollX + (int)(int_screenWidth / 2);
+                    int int_posicionScroll = int_anchoPromedio/int_screenWidth;
+                    int int_moverA = int_posicionScroll * int_screenWidth;
+                    Log.d("DEBUG",String.valueOf(int_moverA));
+                    HSV_pantalla.smoothScrollTo(int_moverA, 0);
+                    runActivo = true;
+                } else {
+                    runActivo = false;
+                }
+            } else {
+                handler.postDelayed(this, 50);
+            }
+        }
+    };
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        r = getResources();
+        int_screenWidth = r.getDisplayMetrics().widthPixels;
+
         View vista_inicio = inflater.inflate(R.layout.fragment_fragmento_casa, container, false);
         HSV_pantalla = vista_inicio.findViewById(R.id.HSV_pantalla);
         ///HISTORIAS
-        int int_historias = (int)Math.floor(Math.random()*(10)+0);
+        int int_historias = (int)Math.floor(Math.random()*(0)+0);
         LinearLayout LL_historias = vista_inicio.findViewById(R.id.LL_historias);
         LinearLayout.LayoutParams LLLP_botonHistorias = new LinearLayout.LayoutParams(
                 LayoutParams.MATCH_PARENT,
@@ -113,32 +142,33 @@ public class Fragmento_casa extends Fragment {
             }
         }
         /// PUBLICACIONES
-        int int_publicaciones = (int)Math.floor(Math.random()*(5)+1);
+        int int_publicaciones = (int)Math.floor(Math.random()*(5)+2);
         String[] String_Lugares = {"Dean & Dennys","McDonald's","Burguer King","Mostaza","Deniro's"};
         String[] String_Personas = {"Darío","Camila","Facundo","Ignacio","Agus"};
-        int int_screenWidth = (90*(Resources.getSystem().getDisplayMetrics().widthPixels))/100;
+        int int_alturaTitulo = Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 45,r.getDisplayMetrics()));
+        int int_alturaFoto = Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 400,r.getDisplayMetrics()));
 
         LinearLayout LL_separador = vista_inicio.findViewById(R.id.LL_separador);
         LinearLayout.LayoutParams LLLP_publicacion = new LinearLayout.LayoutParams(
                 int_screenWidth, LayoutParams.MATCH_PARENT);
-        LLLP_publicacion.setMargins(2,2,20,0);
         LinearLayout.LayoutParams LLLP_publicacionScroll = new LinearLayout.LayoutParams(
                 LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        LLLP_publicacionScroll.setMargins(0,0,0,0);
         LinearLayout.LayoutParams LLLP_titulo = new LinearLayout.LayoutParams(
-                LayoutParams.MATCH_PARENT, 120);
+                LayoutParams.MATCH_PARENT, int_alturaTitulo);
         LLLP_titulo.setMargins(0,0,0,10);
         LinearLayout.LayoutParams LLLP_tvTitulo = new LinearLayout.LayoutParams(
                 LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT, (float)8.0);
         LLLP_tvTitulo.setMargins(10,0,0,0);
         LinearLayout.LayoutParams LLLP_foto = new LinearLayout.LayoutParams(
-                LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-        LLLP_foto.setMargins(0, 20, 0, 20);
+                LayoutParams.MATCH_PARENT, int_alturaFoto);
+        LLLP_foto.setMargins(0, 0, 0, 5);
 
         for (int x=0 ; x<int_publicaciones ; x++ ) {
-            int int_fotos = (int)Math.floor(Math.random()*(5)+1);
-            int int_lugar = (int)Math.floor(Math.random()*(5)+0);
-            int int_persona = (int)Math.floor(Math.random()*(5)+0);
+            int int_fotos = (int) Math.floor(Math.random() * (5) + 1);
+            int int_lugar = (int) Math.floor(Math.random() * (5) + 0);
+            int int_persona = (int) Math.floor(Math.random() * (5) + 0);
             LinearLayout LL_publicacion = new LinearLayout(LL_separador.getContext());
             LL_publicacion.setOrientation(LinearLayout.VERTICAL);
             LL_publicacion.setLayoutParams(LLLP_publicacion);
@@ -150,12 +180,12 @@ public class Fragmento_casa extends Fragment {
             LL_titulo.setLayoutParams(LLLP_titulo);
             LL_titulo.setPadding(15, 10, 0, 0);
             LL_titulo.setBackgroundColor(Color.TRANSPARENT);
-                /// IMAGEN PERFIL
+            /// IMAGEN PERFIL
             ImageView IV_perfil = new ImageView(LL_titulo.getContext());
             IV_perfil.setImageResource(R.drawable.imagenperfil);
-            IV_perfil.setLayoutParams(new LinearLayout.LayoutParams(170,LayoutParams.MATCH_PARENT, (float)1.0));
+            IV_perfil.setLayoutParams(new LinearLayout.LayoutParams(170, LayoutParams.MATCH_PARENT, (float) 1.0));
             LL_titulo.addView(IV_perfil);
-                /// NOMBRE PERFIL
+            /// NOMBRE PERFIL
             TextView TV_titulo = new TextView(LL_titulo.getContext());
             TV_titulo.setBackgroundColor(Color.TRANSPARENT);
             TV_titulo.setText(String_Personas[int_persona]);
@@ -165,7 +195,7 @@ public class Fragmento_casa extends Fragment {
             TV_titulo.setGravity(Gravity.BOTTOM);
             TV_titulo.setLayoutParams(LLLP_tvTitulo);
             LL_titulo.addView(TV_titulo);
-                /// CANTIDAD FOTOS
+            /// CANTIDAD FOTOS
             String String_CantidadFotos = "FOTOS\n(" + String.valueOf(int_fotos) + ")";
             TextView TV_cantidadFotos = new TextView(LL_titulo.getContext());
             TV_cantidadFotos.setBackgroundColor(Color.TRANSPARENT);
@@ -173,13 +203,13 @@ public class Fragmento_casa extends Fragment {
             TV_cantidadFotos.setTextSize(14);
             TV_cantidadFotos.setTextColor(Color.BLACK);
             TV_cantidadFotos.setTypeface(null, Typeface.BOLD);
-            TV_cantidadFotos.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER);
-            TV_cantidadFotos.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT,(float)8.0));
+            TV_cantidadFotos.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER);
+            TV_cantidadFotos.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, (float) 8.0));
             LL_titulo.addView(TV_cantidadFotos);
-                /// MAS OPCIONES
+            /// MAS OPCIONES
             ImageView IV_masOpciones = new ImageView(LL_titulo.getContext());
             IV_masOpciones.setImageResource(R.drawable.masopciones);
-            IV_masOpciones.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT, (float)1.0));
+            IV_masOpciones.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, (float) 1.0));
             LL_titulo.addView(IV_masOpciones);
             LL_publicacion.addView(LL_titulo);
             /// BARRA NOMBRE LUGAR
@@ -189,8 +219,8 @@ public class Fragmento_casa extends Fragment {
             TV_lugar.setTextSize(18);
             TV_lugar.setTextColor(Color.WHITE);
             TV_lugar.setTypeface(null, Typeface.BOLD);
-            TV_lugar.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER);
-            TV_lugar.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+            TV_lugar.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER);
+            TV_lugar.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
             LL_publicacion.addView(TV_lugar);
             /// PANTALLA SCROLL DE FOTOS
             ScrollView SV_publicacion = new ScrollView(LL_publicacion.getContext());
@@ -200,72 +230,50 @@ public class Fragmento_casa extends Fragment {
             LL_divisionFotos.setOrientation(LinearLayout.VERTICAL);
             LL_divisionFotos.setBackgroundColor(Color.TRANSPARENT);
             LL_divisionFotos.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            /// DESCRIPCIÓN
+            int int_descripcion = (int) Math.floor(Math.random() * (4) + 0);
+            String String_descripcion = "Descripción";
+            for (int f = 0; f < int_descripcion; f++) {
+                String_descripcion += "\n. . . .";
+            }
+            TextView TV_descripcion = new TextView(LL_titulo.getContext());
+            TV_descripcion.setBackgroundColor(Color.TRANSPARENT);
+            TV_descripcion.setText(String_descripcion);
+            TV_descripcion.setTextSize(16);
+            TV_descripcion.setPadding(15, 0, 0, 10);
+            TV_descripcion.setTextColor(Color.BLACK);
+            TV_descripcion.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            LL_divisionFotos.addView(TV_descripcion);
             for (int n = 0; n < int_fotos; n++) {
-                int int_descripcion = (int)Math.floor(Math.random()*(4)+0);
-                String String_descripcion = "Descripción";
-                for (int f=0 ; f<int_descripcion ; f++){String_descripcion+="\n. . . .";}
                 LinearLayout LL_foto = new LinearLayout(LL_divisionFotos.getContext());
                 LL_foto.setOrientation(LinearLayout.VERTICAL);
                 LL_foto.setBackgroundColor(Color.TRANSPARENT);
                 LL_foto.setLayoutParams(LLLP_foto);
-                /// DESCRIPCIÓN
-                TextView TV_descripcion = new TextView(LL_titulo.getContext());
-                TV_descripcion.setBackgroundColor(Color.TRANSPARENT);
-                TV_descripcion.setText(String_descripcion);
-                TV_descripcion.setTextSize(16);
-                TV_descripcion.setPadding(15,0,0,10);
-                TV_descripcion.setTextColor(Color.BLACK);
-                TV_descripcion.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
-                LL_foto.addView(TV_descripcion);
                 /// FOTO
                 ImageView IV_foto = new ImageView(LL_titulo.getContext());
                 IV_foto.setImageResource(R.drawable.foto);
-                IV_foto.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT, (float)1.0));
+                IV_foto.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, (float) 1.0));
                 LL_foto.addView(IV_foto);
 
                 LL_divisionFotos.addView(LL_foto);
             }
             SV_publicacion.addView(LL_divisionFotos);
             LL_publicacion.addView(SV_publicacion);
-
             LL_separador.addView(LL_publicacion);
-            /*vista_inicio.setOnDragListener(new View.OnDragListener() {
+
+            HSV_pantalla.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
                 @Override
-                public boolean onDrag(View v, DragEvent event) {
-                    int int_evento = event.getAction();
-                    Log.d("DEBUG", "DRAG-STARTED");
-                    switch(int_evento) {
-                        case DragEvent.ACTION_DRAG_STARTED: {
-                            Log.d("DEBUG", "DRAG-STARTED");
-                        }
-                        case DragEvent.ACTION_DRAG_LOCATION: {
-                            Log.d("DEBUG", "DRAG_LOCATION: " + HSV_pantalla.getScrollX());
-                        }
+                public void onScrollChanged() {
+                    Long Long_temp = System.currentTimeMillis()/300;
+                    runActivo = false;
+                    if(!handler.hasCallbacks(runnable)) {
+                        handler.post(runnable);
                     }
-                    return true;
+                    tsLong = Long_temp;
                 }
             });
+
         }
-        HSV_pantalla.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-            @Override
-            public void onScrollChanged() {
-                Long Long_temp = System.currentTimeMillis()/1000;
-                if(Long_temp-tsLong>1 ) {
-                    tsLong = Long_temp;
-                    int temp_scrollX = HSV_pantalla.getScrollX(); // For HorizontalScrollView
-                    Log.d("DEBUG","Scroll: " + temp_scrollX);
-                    Log.d("DEBUG","Tiempo: " + tsLong.toString());
-                }
-
-                /*if( temp_scrollX>int_scrollX ) {
-                    int_scrollX = int_screenWidth+(int_screenWidth*(temp_scrollX/int_screenWidth));
-                } else {
-                    if( temp_scrollX<int_scrollX ) int_scrollX = -int_screenWidth-(int_screenWidth*(temp_scrollX/int_screenWidth));
-                }
-                HSV_pantalla.setScrollX(int_scrollX);
-            }
-        });*/
-
         return vista_inicio;
     }
 }
